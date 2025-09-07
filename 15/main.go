40e9d98441882eda1f99101c26be545938b44457
Eritsian David <wruser123@gmail.com>
2025-09-07 13:27:29 +0300

    m

---
+++ 15/main.go
@@ -0,0 +1,30 @@
+package main
+
+import "strings"
+
+func createHugeString(size int) string {
+	return strings.Repeat("a", size)
+}
+
+// глобальная переменная
+// живат в памяти до тех пор, пока программа жива
+var justString string
+
+func someFunc() {
+	// создаем большую строку (размером 1024)
+	v := createHugeString(1 << 10)
+	// теперь justString ссылается на тот же массив байтов, что и v
+	justString = v[:100]
+	// после окончания работы someFunc() в памяти останется весь массив 1024 байтов,
+	// так как justString ссылается на него и зборщик мусора не сможет удалить его
+	// пока жива переменная justString
+
+	// правильно будет сделать сделать копию
+	// justString = string([]byte(v[:100]))
+	// или
+	// justString = strings.Clone(v[:100])
+}
+
+func main() {
+	someFunc()
+}
